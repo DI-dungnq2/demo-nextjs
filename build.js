96c513d47aba269faefa92219cf7ad7e1f452aac
DI-dungnq2 <nguyen.quang.dung@dataimpact.vn>
2025-08-28 10:35:19 +0700

    update

--- build.js
+++
@@ -1,9 +0,0 @@
-const esbuild = require("esbuild");
-
-esbuild.build({
-  entryPoints: ["./scripts/worker.ts"],
-  bundle: true,
-  minify: true,
-  platform: "browser",
-  outfile: "public/worker.js",
-}).catch(() => process.exit(1));
--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 const nextConfig = {
   output: "export",
-  basePath: "/static-export-demo",
-  assetPrefix: "/static-export-demo"
 };
 
 module.exports = nextConfig;
